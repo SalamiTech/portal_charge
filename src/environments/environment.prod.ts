d8a15f5f17067ae77af275988395006957d7593d
SalamiTech <salamigideon01@gmail.com>
2023-10-07 07:59:46 +0000

    environment fixes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
-// export const environment = {
-//   production: true,
-//   app: 'MOBY LEGAL',
-//   countryCode: "GH",
-//   appUrl: "/s2/moby_legal_letters_apis",
-//   uploadUrl: "/s2",
-//   authUrl: "/auth",
-//   assetsUrl: "/businessAccountOpening",
-//   "X-IBM-Client-Id": "25bea38f-8aa5-4f7b-b50b-b9ab6ea6c830",
-//   "X-IBM-Client-Secret": "jE1yI1pS6rP0kX1xT2pP3hQ6bY7mX1jJ3rW2xR8lN0hM7wX7tH",
-//   requestToken: "jE1yI1pS6rP0kX1xT2pP3hQ6bY7mX1jJ3rW2xR8lN0hM7wX7tH"
-// };
+export const environment = {
+  production: true,
+  app: 'MOBY LEGAL',
+  countryCode: "GH",
+  appUrl: "/s2/moby_legal_letters_apis",
+  uploadUrl: "/s2",
+  authUrl: "/auth",
+  assetsUrl: "/businessAccountOpening",
+  "X-IBM-Client-Id": "25bea38f-8aa5-4f7b-b50b-b9ab6ea6c830",
+  "X-IBM-Client-Secret": "jE1yI1pS6rP0kX1xT2pP3hQ6bY7mX1jJ3rW2xR8lN0hM7wX7tH",
+  requestToken: "jE1yI1pS6rP0kX1xT2pP3hQ6bY7mX1jJ3rW2xR8lN0hM7wX7tH"
+};
